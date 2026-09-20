b06a1fa2a7061267c3235ee28023295a48b5bc8c
Brilly Arfendo Gautama <52552859+ae-junior@users.noreply.github.com>
2026-09-20 13:32:43 +0700

    Refactor smooth scrolling in scripts.js
    Updated smooth scrolling functionality to adjust for fixed header.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
-$(document).ready(function(){
-    // You can add jQuery functionality here if needed.
-    // Example: Smooth scrolling for anchor links
-    $('a[href*="#"]').on('click', function(e) {
-        e.preventDefault();
-        
-        var target = $(this.hash);
-        $('html, body').animate({
-            scrollTop: target.offset().top
-        }, 800); // The number here represents the speed of the scroll in milliseconds.
-    });
+$(document).ready(function () {
+  // Smooth scrolling for navigation links
+  $('a[href*="#"]').on("click", function (e) {
+    e.preventDefault();
+
+    var target = $(this.hash);
+    $("html, body").animate(
+      {
+        scrollTop: target.offset().top - 60,
+      },
+      500,
+    );
+  });
 });
